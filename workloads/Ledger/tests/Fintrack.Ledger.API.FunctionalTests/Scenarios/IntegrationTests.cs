b4eb8bd02529675c2b3794236ece488b17b1f4da
Gabriel Abreu <gabriel@gabbium.dev>
2025-09-26 22:23:15 -0300

    test: debug

--- workloads/Ledger/tests/Fintrack.Ledger.API.FunctionalTests/Scenarios/IntegrationTests.cs
+++ workloads/Ledger/tests/Fintrack.Ledger.API.FunctionalTests/Scenarios/IntegrationTests.cs
@@ -7,34 +7,29 @@ public class IntegrationTests
     {
         // Arrange
         var builder = await DistributedApplicationTestingBuilder
-            .CreateAsync<Projects.Fintrack_AppHost>();
+            .CreateAsync<Projects.Fintrack_Ledger_API>(TestContext.Current.CancellationToken);
 
         builder.Services.ConfigureHttpClientDefaults(clientBuilder =>
         {
             clientBuilder.AddStandardResilienceHandler();
         });
 
-        await using var app = await builder.BuildAsync();
+        await using var app = await builder.BuildAsync(TestContext.Current.CancellationToken);
 
-        await app.StartAsync();
+        await app.StartAsync(TestContext.Current.CancellationToken);
 
         // Act
         var httpClient = app.CreateHttpClient("ledger-api");
 
         using var cts = new CancellationTokenSource(TimeSpan.FromSeconds(30));
-        await app.ResourceNotifications.WaitForResourceHealthyAsync(
-            "ledger-api",
-            cts.Token);
 
-        var response = await httpClient.GetAsync("/");
+        //await app.ResourceNotifications.WaitForResourceHealthyAsync(
+        //    "ledger-api",
+        //    cts.Token);
+
+        var response = await httpClient.GetAsync("/", TestContext.Current.CancellationToken);
 
         // Assert
         Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-    }
-
-    [Fact]
-    public void Bro()
-    {
-        Assert.True(true);
     }
 }
