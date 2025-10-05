f161ba8e8596b4e5019d88a4a2561c4b2c3c9abe
Gabriel Abreu <gabriel@gabbium.dev>
2025-10-05 20:01:55 -0300

    feat: enterprise workflows (#12)
    * chore: subfolders
    
    * feat: add pr workflows
    
    * fix: working dir terraform
    
    * fix: add ci auto tfvars
    
    * fix: format
    
    * fix: add permissions
    
    * feat: add release / deploy workflows

---
+++ backend/services/Ledger/tests/Fintrack.Ledger.Api.FunctionalTests/TestHelpers/Infrastructure/CustomWebApplicationFactory.cs
@@ -0,0 +1,31 @@
+﻿using Fintrack.Ledger.Api;
+using Fintrack.Ledger.Api.FunctionalTests.TestHelpers.Infrastructure.Authorize;
+
+namespace Fintrack.Ledger.Api.FunctionalTests.TestHelpers.Infrastructure;
+
+public class CustomWebApplicationFactory(string connectionString) : WebApplicationFactory<Program>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.UseSetting("Authentication:OidcJwt:Authority", "http://localhost");
+        builder.UseSetting("ConnectionStrings:LedgerDb", connectionString);
+
+        builder.ConfigureServices(services =>
+        {
+            services.AddSingleton<IAutoAuthorizeAccessor, AutoAuthorizeAccessor>();
+            services.AddSingleton<IStartupFilter>(new AutoAuthorizeStartupFilter());
+        });
+    }
+
+    private class AutoAuthorizeStartupFilter : IStartupFilter
+    {
+        public Action<IApplicationBuilder> Configure(Action<IApplicationBuilder> next)
+        {
+            return builder =>
+            {
+                builder.UseMiddleware<AutoAuthorizeMiddleware>();
+                next(builder);
+            };
+        }
+    }
+}
